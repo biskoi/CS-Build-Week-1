ade8b5b6ce25cf63defb461b7e2419bcd20b3f63
biskoi <biskoi@daydream.cafe>
2020-07-30 10:26:02 -0600

    Functionality MVP, working on writing part

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -21,6 +21,8 @@ width: 15px;
 // border: solid pink 1px
 `;
 
+let interval;
+
 function App() {
   
   const colNums = 40;
@@ -35,40 +37,15 @@ function App() {
     return arr
   };
 
-  let grid = make2DArray(colNums, rowNums);
-  
+  let grid = make2DArray(colNums, rowNums); 
   
   const [gridData, setGridData] = useState(grid);
   const [running, setRunning] = useState(false);
-  const runningRef = useRef();
-  runningRef.current = running;
+  const [gen, setGen] = useState(0)
+  const [mode, setMode] = useState('TOGGLE');
   const gridRef = useRef();
   gridRef.current = gridData;
 
-
-
-  const clearGrid = () => {
-    setGridData(grid)
-  }
-
-  const cellToggle = (e, i, j) => {
-    const newGrid = [...gridRef.current];
-    if (gridRef.current[i][j]) {
-      let split = e.target.className.split(' ');
-      e.target.className = `${split[0]} ${split[1]}`;
-      newGrid[i][j] = 0;
-
-    } else if (!gridRef.current[i][j]) {
-      e.target.className = `${e.target.className} ${'filled'}`;
-      newGrid[i][j] = 1;
-    };
-
-    setGridData(newGrid)
-    console.table(newGrid)
-    console.log(i, j, 'val:', newGrid[i][j])
-    console.log(newGrid[i])
-
-  };
 
 
   const gridStep = () => {
@@ -77,7 +54,7 @@ function App() {
     // [0, 39] = top right
     // [39, 39] = bottom right
     // [39, 0] = bottom left
-
+    setGen(g => g + 1)
     const operations = [
       [0, -1],
       [-1, -1],
@@ -112,7 +89,7 @@ function App() {
           // if cell is alive already
           if (neighbors < 2 || neighbors > 3) {
             nextGrid[i][j] = 0
-            console.log('killed cell', i, j, neighbors)
+            // console.log('killed cell', i, j, neighbors)
           }else if (neighbors === 2 || neighbors === 3) {
             nextGrid[i][j] = 1
           }
@@ -127,26 +104,50 @@ function App() {
     // console.log('new grid after ops', nextGrid)
     setGridData(nextGrid)
   };
-
-  let interval;
-
+  
   const startRender = () => {
-    // gridStep();
-    interval = setInterval(gridStep, 500)
-  }
+    gridStep();
+    interval = setInterval(gridStep, 200);
+    console.log(`running from loop is ${running}`);
+  };
 
-  const toggleStart = () => {
-    console.log('togglestart')
-    if (running) {
-      console.log('stopping')
-      setRunning(false)
-      clearInterval(interval)
-    } else if (!runningRef.current) {
-      console.log('starting')
+  const start = () => {
+    if (!running) {
+
       setRunning(true)
       startRender();
+    };
+
+  };
+
+  const stop = () => {
+    console.log(`stopping`)
+    setRunning(false)
+    clearInterval(interval)
+  };
+
+  const cellToggle = (i, j) => {
+
+    if (running) {
+      return;
     }
-  }
+    const newGrid = [...gridRef.current];
+    if (gridRef.current[i][j]) {
+      // let split = e.target.className.split(' ');
+      // e.target.className = `${split[0]} ${split[1]}`;
+      newGrid[i][j] = 0;
+
+    } else if (!gridRef.current[i][j]) {
+      // e.target.className = `${e.target.className} ${'filled'}`;
+      newGrid[i][j] = 1;
+    };
+
+    setGridData(newGrid)
+    console.table(newGrid)
+    console.log(i, j, 'val:', newGrid[i][j])
+    console.log(newGrid[i])
+
+  };
 
   const fill = (i, j) => {
     let fillGrid = [...gridData];
@@ -173,6 +174,100 @@ function App() {
       };
     });
     setGridData(fillGrid)
+  };
+
+  const clearGrid = () => {
+    stop();
+    setGridData(grid)
+    setGen(0)
+  };
+
+  const randomFill = () => {
+    const randomGrid = make2DArray(colNums, rowNums);
+    for (let i = 0; i < 40; i++) {
+      for (let j = 0; j < 40; j++) {
+        let num = Math.random()
+        if (num > 0.75) {
+          randomGrid[i][j] = 1
+        }
+      };
+    };
+    setGridData(randomGrid);
+    setGen(0)
+  };
+
+  const glider = (i, j) => {
+    let gliderGrid = [...gridData];
+
+    const gliderOps = [
+      [0, 1],
+      [-1, 0],
+      [1, -1],
+      [1, 0],
+      [1, 1]
+    ];
+
+    gliderOps.forEach(([x, y]) => {
+      let newX = i + x;
+      let newY = j + y;
+
+      if (newX >= 0 && newX <= 39 && newY >= 0 && newY <= 39) {
+        gliderGrid[newX][newY] = 1;
+      };
+
+    });
+
+    setGridData(gliderGrid)
+  };
+
+  const exploder = (i, j) => {
+    let exploderGrid = [...gridData];
+
+    const exploderOps = [
+      [-2, -2],
+      [0, -2],
+      [2, -2],
+      [-2, -1],
+      [2, -1],
+      [-2, 0],
+      [2, 0],
+      [-2, 1],
+      [2, 1],
+      [0, 2],
+      [2, 2],
+      [-2, 2],
+    ];
+
+    exploderOps.forEach(([x, y]) => {
+      let newX = x + i;
+      let newY = y + j;
+
+      if (newX >= 0 && newX <= 39 && newY >= 0 && newY <= 39) {
+        exploderGrid[newX][newY] = 1;
+      };
+    });
+
+    setGridData(exploderGrid);
+  };
+
+  const modeHandler = (i, j) => {
+    switch (mode) {
+
+      case 'TOGGLE':
+        return cellToggle(i, j);
+    
+      case 'BORDER_FILL':
+        return fill(i, j);
+
+      case 'GLIDER':
+        return glider(i, j);
+
+      case 'EXPLODER':
+        return exploder(i, j);
+
+      default:
+        break;
+    }
   }
 
 
@@ -185,15 +280,21 @@ function App() {
         {gridData.map((item, i) => {
           return item.map((subItem, j) => {
             // return <Cell className = {subItem ? 'filled' : undefined} onClick = {(e) => fill(i, j)}>
-            return <Cell className = {subItem ? 'filled' : undefined} onClick = {(e) => cellToggle(e, i, j)}>
+            return <Cell className = {subItem ? 'filled' : undefined} onClick = {(e) => modeHandler(i, j)}>
             {/* {subItem ? subItem : 0} */}
             </Cell>
           })
         })}
       </Gridcontainer>
+      <p>
+        {`Generation: ${gen}`}
+      </p>
       <div>
-        <button onClick = {toggleStart}>
-          {running ? 'Stop' : 'Start'}
+        <button onClick = {start}>
+          Start
+        </button>
+        <button onClick = {stop}>
+          Stop
         </button>
         <button onClick = {gridStep}>
           Step
@@ -201,8 +302,28 @@ function App() {
         <button onClick = {clearGrid}>
           Clear
         </button>
+        <button onClick = {randomFill}>
+          Random
+        </button>
         <button onClick = {() => {console.log(gridData)}}>
           log
+        </button>
+      </div>
+      <p>
+        Fill Mode: {`${mode}`}
+      </p>
+      <div>
+        <button onClick = {() => {setMode('TOGGLE')}}>
+          Toggle
+        </button>
+        <button onClick = {() => {setMode('BORDER_FILL')}}>
+          Border Fill
+        </button>
+        <button onClick = {() => {setMode('GLIDER')}}>
+          Glider
+        </button>
+        <button onClick = {() => {setMode('EXPLODER')}}>
+          Exploder
         </button>
       </div>
     </div>
