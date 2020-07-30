20400bed4d7fde426506028d91fec483b3962790
biskoi <biskoi@daydream.cafe>
2020-07-30 16:00:24 -0600

    Updates

---
+++ precache-manifest.e64338f0ab9a813f8107224531c38569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69553372e6fda7345e75d8e62feca191",
+    "url": "/CS-Build-Week-1/index.html"
+  },
+  {
+    "revision": "b4f0e7ec4fb6648c1918",
+    "url": "/CS-Build-Week-1/static/css/main.23ee82bc.chunk.css"
+  },
+  {
+    "revision": "7e49a949d26f4dfba4b5",
+    "url": "/CS-Build-Week-1/static/js/2.ef7c0d82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CS-Build-Week-1/static/js/2.ef7c0d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f0e7ec4fb6648c1918",
+    "url": "/CS-Build-Week-1/static/js/main.9dfc5619.chunk.js"
+  },
+  {
+    "revision": "876fea3487a6808bc0b9",
+    "url": "/CS-Build-Week-1/static/js/runtime-main.cea90ff6.js"
+  }
+]);
